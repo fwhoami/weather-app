5c853dbbe015f13f2e47267caa4ba102286bcf07
Kate <clahfclns@gmail.com>
2022-07-16 17:35:22 +0300

    fixing

--- src/script.js
+++ src/script.js
@@ -37,3 +37,31 @@ function formatDate(date){
 let now = new Date();
 let dateElement = document.querySelector("#current-date");
 dateElement.innerHTML = formatDate(now);
+
+function searchSubmit(event){
+  event.preventDefault();
+  let cityElement = document.querySelector("#city-search");
+  cityElement.innerHTML = cityInput.value;
+}
+let searchCity = document.querySelector("#city-search-form");
+searchCity.addEventListener("submit", searchCity);
+
+
+//Temperature
+function getFahrenheitTemp(event) {
+  event.preventDefault();
+  let currentTemp = document.querySelector("#main-temperature");
+  currentTemp.innerHTML = `50°`;
+}
+
+function getCelsiusTemp(event) {
+  event.preventDefault();
+  let currentTemp = document.querySelector("#main-temperature");
+  currentTemp.innerHTML = `17°`;
+}
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", getFahrenheitTemp);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", getCelsiusTemp);
