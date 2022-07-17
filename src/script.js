64e24ccc9425c40b5da2371a3010d3cdebfc0b0f
Kate <clahfclns@gmail.com>
2022-07-17 21:17:09 +0300

    more fixing

--- src/script.js
+++ src/script.js
@@ -34,17 +34,50 @@ function formatDate(date){
     return `${day}, ${currentDate} ${month}`;
 }
 
+
+function formatTime(date) {
+  let hour = now.getHours();
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hour}:${minutes}`;
+}
+
+
 let now = new Date();
 let dateElement = document.querySelector("#current-date");
 dateElement.innerHTML = formatDate(now);
+let timeElement = document.querySelector("#current-time");
+timeElement.innerHTML = formatTime(now);
+
+
+
+function searchCity(city){
+  let apiKey = "32b1356da0b65f877b0f297ff829102a";
+  let unit = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function displayWeather(response){
+  document.querySelector("#current-city").innerHTML = response.data.name;
+  document.querySelector("#main-temperature") = `${Math.round(
+    response.data.main.temp
+  )}°`;
+}
 
 function searchSubmit(event){
   event.preventDefault();
-  let cityElement = document.querySelector("#city-search");
-  cityElement.innerHTML = cityInput.value;
+  let city = document.querySelector("#current-city").value;
+  searchCity(city);
 }
-let searchCity = document.querySelector("#city-search-form");
-searchCity.addEventListener("submit", searchCity);
+
+let searchForm = document.querySelector("#city-search-form");
+searchForm.addEventListener("submit", searchSubmit);
+
+searchCity("Tokyo");
 
 
 //Temperature
@@ -65,3 +98,5 @@ fahrenheitLink.addEventListener("click", getFahrenheitTemp);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", getCelsiusTemp);
+
+
